8befe3241aa11fdc12d83adbfd6410ac5b9d85ea
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-02-22 16:30:01 +0900

    Modify controller to fetch phone data by XHR

--- web/main.dart
+++ web/main.dart
@@ -7,21 +7,15 @@ import 'package:angular/angular.dart';
   publishAs: 'ctrl'
 )
 class PhoneListCtrl {
-  List<Map> phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age' : 1,
-    },
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age' : 2,
-    },
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age' : 3,
-    },
-  ];
-  String orderProp = "age";
+    HTTP _http;
+    List<Map> phones = [];
+    String orderProp = "age";
+
+    PhoneListCtrl(this._http) {
+        this._http.get("./phones/phones.json").then((resp) {
+            this.phones = resp.data;
+        });
+    }
 }
 
 main() {
