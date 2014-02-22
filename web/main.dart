e81bbe6893bc204959a9d1b8302052d46153b703
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-02-22 15:34:03 +0900

    Add orderBy filter

--- web/main.dart
+++ web/main.dart
@@ -8,13 +8,20 @@ import 'package:angular/angular.dart';
 )
 class PhoneListCtrl {
   List<Map> phones = [
-{'name': 'Nexus S',
- 'snippet': 'Fast just got faster with Nexus S.'},
-{'name': 'Motorola XOOM™ with Wi-Fi',
- 'snippet': 'The Next, Next Generation tablet.'},
-{'name': 'MOTOROLA XOOM™',
- 'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age' : 1,
+    },
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age' : 2,
+    },
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age' : 3,
+    },
   ];
+  String orderProp = "age";
 }
 
 main() {
