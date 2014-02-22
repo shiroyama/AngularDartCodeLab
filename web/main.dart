7f195de6247876f3dfc18fb2af0549e31d40513a
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-02-22 14:58:06 +0900

    First dart app

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,23 @@
 library phonecat;
 
 import 'package:angular/angular.dart';
-main() => ngBootstrap();
+
+@NgController(
+  selector: '[phone-list]',
+  publishAs: 'ctrl'
+)
+class PhoneListCtrl {
+  List<Map> phones = [
+{'name': 'Nexus S',
+ 'snippet': 'Fast just got faster with Nexus S.'},
+{'name': 'Motorola XOOM™ with Wi-Fi',
+ 'snippet': 'The Next, Next Generation tablet.'},
+{'name': 'MOTOROLA XOOM™',
+ 'snippet': 'The Next, Next Generation tablet.'},
+  ];
+}
+
+main() {
+  Module phoneCatModule = new Module()..type(PhoneListCtrl);
+  ngBootstrap(module: phoneCatModule);
+}
